bd6ba3be6fe47f94bd8d5c17db8f9858f5d50c0b
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-01 12:02:31 +0200

    Create urls.py

---
+++ backend/config/urls.py
@@ -0,0 +1,8 @@
+#backend/config/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("logistics/", include("logistics.urls", namespace="logistics")),
+]
