fbc9cb813b7f0614e724b8467730856d00fec423
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-01 15:18:39 +0200

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Running migrations..."
 python manage.py migrate
-
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-echo "Starting Gunicorn server..."
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
