adaa8a7ac7fd4378b44bd211c62c48c568112006
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-01 14:13:41 +0200

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("logistics/", include("logistics.urls", namespace="logistics")),
+    path("vite/", include("django_vite.urls")),
 ]
