d0ce3c3bb38667a9dc2560a1722c9663284a4ba4
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-03 16:13:14 +0200

    Update urls.py

--- backend/logistics/urls.py
+++ backend/logistics/urls.py
@@ -1,9 +1,13 @@
 #backend/logistics/urls.py
 from django.urls import path
 from .views import CheckDeltaView 
+from logistics.views import CheckDeltaView, UploadInvoiceFile, TaskStatusView, TaskResultView
 
 app_name = "logistics"
 
 urlpatterns = [
+    path("upload/", UploadInvoiceFile.as_view(), name="upload-invoice"),
     path("check-delta/", CheckDeltaView.as_view(), name="check-delta"),
+    path("task-status/", TaskStatusView.as_view(), name="task-status"),
+    path("task-result/", TaskResultView.as_view(), name="task-result"),
 ]
