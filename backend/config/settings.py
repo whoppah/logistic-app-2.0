331895222e43f48913d6b235fd619c68d778e265
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-03 11:50:33 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -108,14 +108,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-# Static + media
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "frontend" / "dist",  
-]
-
+# Media
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
