41ea720c892f9e811906d17f029cc2e3f1e09fc4
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-03 12:25:24 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public', 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/main.jsx'),
-    },
-  },
   server: {
-    port: 5173,
-    strictPort: true,
-    host: true,
-  }
-});
+    proxy: {
+      '/api': 'http://localhost:8000', 
+    },
+  },
+})
