a9c0c36b5f7ca3ace4cf0623ad864b74af4feef0
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-03 11:54:13 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,17 +10,16 @@ export default defineConfig(({ command }) => ({
     },
   },
   build: {
-    outDir: 'dist',                 
-    manifest: true,                 
-    emptyOutDir: true,             
+    outDir: 'dist',
+    emptyOutDir: true,
+    manifest: true,
     rollupOptions: {
       input: path.resolve(__dirname, 'src/main.jsx'),
     },
   },
   server: {
-    host: 'localhost',
     port: 5173,
-    origin: 'http://localhost:5173',
-    strictPort: true,               
-  },
-}));
+    strictPort: true,
+    host: true, 
+  }
+});
