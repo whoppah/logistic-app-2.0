ab1e8137e12e45275414c35ae3ce1538a789726d
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-07 11:44:18 +0200

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -15,7 +15,7 @@ case "$1" in
     exec gunicorn config.wsgi:application \
       --bind 0.0.0.0:"${PORT:-8000}" \
       --workers 2 \
-      --timeout 120 \
+      --timeout 300 \
       --log-level info
     ;;
   worker)
