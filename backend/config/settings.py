f5fbf7c40473781e75760faebe9ab65ba58cace2
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-07 10:47:28 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -17,7 +17,6 @@ GOOGLE_SERVICE_ACCOUNT_FILE = Path(
 SECRET_KEY = config("SECRET_KEY", default="insecure-key")
 DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = [
-    "web-production-213b4.up.railway.app",
     "logistics-app-20-or-backend-production.up.railway.app",
     "logistic-app-20-or-frontend-production.up.railway.app"
 ]
