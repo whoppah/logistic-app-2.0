cc33e21b43ec20ba912b3578ae398d394ac7b575
mbiancotto <151370705+mbiancotto@users.noreply.github.com>
2025-07-16 15:15:22 +0200

    Update wuunder.py

--- backend/logistics/parsers/wuunder.py
+++ backend/logistics/parsers/wuunder.py
@@ -84,7 +84,7 @@ class WuunderParser(BaseParser):
                 m = re.search(r"Factuurnummer[:\s]+(\d+)", line)
                 if m:
                     invoice_number = m.group(1)
-                    print("[DEBUG] Parsed invoice number:", invoice_number)
+                    #print("[DEBUG] Parsed invoice number:", invoice_number)
                     
             if not invoice_date and "Factuurdatum" in line:
                 m = re.search(r"Factuurdatum[:\s]*(\d{1,2}\s+\w+\s+\d{4})", line, flags=re.IGNORECASE)
